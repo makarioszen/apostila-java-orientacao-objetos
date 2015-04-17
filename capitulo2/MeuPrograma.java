d23006ec323eb71da76b6dae1f15971a755d7248
Marco Antonio <marcoamj1982@gmail.com>
2015-04-17 20:11:03 -0300

    2.14 - Exercícios: Modificando o Hello World

--- capitulo2/MeuPrograma.java
+++ capitulo2/MeuPrograma.java
@@ -1,6 +1,5 @@
 class MeuProgama{
 	public static void main(String[] args){
-		System.out.println("O Marco é um cara legal");
-		System.out.println("Duvida?");
+		System.out.println("O Marco é um cara legal\nDuvida?");
 	}
 }
